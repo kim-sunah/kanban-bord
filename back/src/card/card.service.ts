7aeb443854aa105eeef1ab416bf260dc4199b700
dabbler2 <117023230+dabbler2@users.noreply.github.com>
2024-01-05 18:31:13 +0900

    코드 정리

--- back/src/card/card.service.ts
+++ back/src/card/card.service.ts
@@ -42,89 +42,71 @@ export class CardService {
 
   // 특정 카드 찾기
   async findCard(cardSeq: number) {
-    return await this.cardRepository.findOne({ where: { cardSeq } });
+    const card = await this.cardRepository.findOne({ where: { cardSeq } });
+	if (!card) throw new NotFoundException('해당 카드를 찾을 수 없습니다.');
+    return card;
   }
 
   // 카드 정보 수정
   async updateCard(updateCardDto: UpdateCardDto, cardSeq: number) {
     await this.cardRepository.update({ cardSeq }, updateCardDto);
-    const card = await this.findCard(cardSeq);
-    if (!card) throw new NotFoundException('해당 카드를 찾을 수 없습니다.');
-    return card;
+    return await this.findCard(cardSeq);
+  }
+  
+  // 카드 포지션 바꾸기
+  async swap(card: any, columnSeq: number, position: number, newPosition: number){
+	const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      card.position = -1;
+      await queryRunner.manager.save(card);
+      await queryRunner.manager.update(
+        Card,
+        { columnSeq, position: newPosition },
+        { position },
+      );
+      card.position = newPosition;
+      await queryRunner.manager.save(card);
+      await queryRunner.commitTransaction();
+    } catch (e) {
+      await queryRunner.rollbackTransaction();
+      throw e;
+    } finally {
+      await queryRunner.release();
+    }
   }
 
   // 컬럼에서 카드 한칸 위로 올리기
   async cardUp(cardSeq: number) {
     const card = await this.findCard(cardSeq);
-    if (!card) throw new NotFoundException('해당 카드를 찾을 수 없습니다.');
     const { position, columnSeq } = card;
     console.log(columnSeq, position);
-    const { prevPosition } = await this.cardRepository
+    const { newPosition } = await this.cardRepository
       .createQueryBuilder('card')
-      .select('MAX(card.position)', 'prevPosition')
+      .select('MAX(card.position)', 'newPosition')
       .where('card.columnSeq = :id', { id: columnSeq })
       .andWhere('card.position < :pos', { pos: position })
       .getRawOne();
-    if (!prevPosition)
+    if (!newPosition)
       throw new BadRequestException('카드가 이미 맨 위에 있습니다.');
-    // 카드 포지션 바꾸는 트랜잭션
-    const queryRunner = this.dataSource.createQueryRunner();
-    await queryRunner.connect();
-    await queryRunner.startTransaction();
-    try {
-      card.position = -1;
-      await queryRunner.manager.save(card);
-      await queryRunner.manager.update(
-        Card,
-        { columnSeq, position: prevPosition },
-        { position },
-      );
-      card.position = prevPosition;
-      await queryRunner.manager.save(card);
-      await queryRunner.commitTransaction();
-    } catch (e) {
-      await queryRunner.rollbackTransaction();
-      throw e;
-    } finally {
-      await queryRunner.release();
-    }
+    this.swap(card,columnSeq,position,newPosition)
   }
 
   // 컬럼에서 카드 한칸 아래로 내리기
   async cardDown(cardSeq: number) {
     const card = await this.findCard(cardSeq);
-    if (!card) throw new NotFoundException('해당 카드를 찾을 수 없습니다.');
     const { position, columnSeq } = card;
     console.log(columnSeq, position);
-    const { nextPosition } = await this.cardRepository
+    const { newPosition } = await this.cardRepository
       .createQueryBuilder('card')
-      .select('MIN(card.position)', 'nextPosition')
+      .select('MIN(card.position)', 'newPosition')
       .where('card.columnSeq = :id', { id: columnSeq })
       .andWhere('card.position > :pos', { pos: position })
       .getRawOne();
-    if (!nextPosition)
+    if (!newPosition)
       throw new BadRequestException('카드가 이미 맨 아래에 있습니다.');
-    // 카드 포지션 바꾸는 트랜잭션
-    const queryRunner = this.dataSource.createQueryRunner();
-    await queryRunner.connect();
-    await queryRunner.startTransaction();
-    try {
-      card.position = -1;
-      await queryRunner.manager.save(card);
-      await queryRunner.manager.update(
-        Card,
-        { columnSeq, position: nextPosition },
-        { position },
-      );
-      card.position = nextPosition;
-      await queryRunner.manager.save(card);
-      await queryRunner.commitTransaction();
-    } catch (e) {
-      await queryRunner.rollbackTransaction();
-      throw e;
-    } finally {
-      await queryRunner.release();
-    }
+    this.swap(card,columnSeq,position,newPosition)
   }
 
   // 카드 다른 컬럼으로 이동
