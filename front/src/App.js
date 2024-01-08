4eb0c1ef0e6db73f92816e48c38d0fc016a08685
sunah <1004_sunah@naver.com>
2024-01-09 03:59:40 +0900

    login

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,24 @@
 
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Boardmain from "./components/board/Board_main";
 import Boardbody from "./components/board/Board_body";
-const router= createBrowserRouter([
-  {path : "/" , element : <Boardmain></Boardmain>,children :[
-    {path : ":id/:title" ,element : <Boardbody></Boardbody>}
-  ]}
-  
+import SignUp from "./components/signUp/signUp";
+import SignIn from "./components/signIn/signIn";
+import Login from "./components/auth/Login";
+const router = createBrowserRouter([
+  {
+    path: "/", element: <Boardmain></Boardmain>, children: [
+      { path: ":id/:title", element: <Boardbody></Boardbody> },
+    ]
+  },
+  { path: "login", element: <Login></Login> },
+  {
+    path: "signUp", children: [
+      { index: true, element: <SignIn></SignIn> },
+      { path: "Signup", element: <SignUp></SignUp> }
+    ]
+  },
+
 ])
 
 function App() {
